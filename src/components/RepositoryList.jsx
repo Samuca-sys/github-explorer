8b8c4b027992ae0017bc38bc137965eb3ea0a97b
Samuel Macedo <samuel.mferreira@hotmail.com>
2021-03-08 21:14:37 -0300

    add repositoryList component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,37 @@
+const repositoryName = 'unform';
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Form inReactJS</p>
+
+          <a href="">
+            Acessar repositórios
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Form inReactJS</p>
+
+          <a href="">
+            Acessar repositórios
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Form inReactJS</p>
+
+          <a href="">
+            Acessar repositórios
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
