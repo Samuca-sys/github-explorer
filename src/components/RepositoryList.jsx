8e4a622b2f62dcc8c73297eeb4a75a3dcbdc43c9
Samuel Macedo <samuel.mferreira@hotmail.com>
2021-03-09 08:40:45 -0300

    add github api access hook

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
@@ -9,6 +10,17 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  // Acessa a api do github, recebe resposta, 
+  // converte para json, e seta resposta de data,
+  // na primeira vez que o componente for exibido em tela
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
